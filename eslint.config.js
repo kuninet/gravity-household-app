48f6076e8d5fea4d9072bcaf6b9c29e2313b9c82
kuninet <kuninet@gmail.com>
2026-09-21 10:25:51 +0900

    chore: husky・lint-stagedおよびPrettier/ESLintによるPre-commit環境を構築

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/uploads/**',
+      '*.db*',
+      'coverage/**',
+      'test-results/**',
+      'playwright-report/**',
+    ],
+  },
+]
